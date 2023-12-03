c9fbb82003ac08f95d2b5212d28593c7d66326b7
Tparfaite <parfaitetwagira@gmail.com>
2023-12-03 14:41:54 +0200

    ng-stepper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CdkStepperModule} from '@angular/cdk/stepper';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StepperComponent } from './components/stepper/stepper.component';
+import { InformationComponent } from './components/information/information.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { SecurityComponent } from './components/security/security.component';
+import { FinishComponent } from './components/finish/finish.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepperComponent,
+    InformationComponent,
+    ContactsComponent,
+    SecurityComponent,
+    FinishComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CdkStepperModule
   ],
+  exports: [CdkStepperModule],
   providers: [],
   bootstrap: [AppComponent]
 })
